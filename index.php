c9af10f2bf53c81e2834fd74c8eb81e5e28b5039
David Joseph Candelaria <36684472+Dc-devRex@users.noreply.github.com>
2020-01-08 21:08:14 -0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,38 @@
+<?php
+
+?>
+    
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <form>
+      <p>
+      <label for="firstName">First Name</label>
+      <input type="text" name="firstName" id="firstName" />
+      </p>
+
+      <p>
+      <label for="lastName">Last Name</label>
+      <input type="text" name="lastName" id="lastName" />
+      </p>
+
+      <p>
+      <label for="email">Email</label>
+      <input type="text" name="email" id="email" />
+      </p>
+
+      <p>
+      <label for="password">Password</label>
+      <input type="password" name="password" id="password" />
+      </p>
+      
+      <input type="submit" value="Submit" />
+    </form>
+</body>
+</html>
